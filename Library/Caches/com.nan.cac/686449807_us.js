bf9b6c24df9a9e0d70ab9845d63049c3648e4343
GitHub Action <action@github.com>
2024-07-31 16:29:36 +0000

    save caches

--- Library/Caches/com.nan.cac/686449807_us.js
+++ Library/Caches/com.nan.cac/686449807_us.js
@@ -3,7 +3,7 @@
   "bundle": "ph.telegra.Telegraph",
   "id": 686449807,
   "name": "Telegram Messenger",
-  "version": "10.14.2",
+  "version": 10.15,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/bd/e8/15/bde81571-2158-dba8-b061-dfd313747493/AppIconLLC-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
