6a4c031abc05ea2d40886c0a859906824e532ce5
GitHub Action <action@github.com>
2024-09-09 22:47:16 +0000

    save caches

--- Library/Caches/com.nan.cac/1523037824_cn.js
+++ Library/Caches/com.nan.cac/1523037824_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.miHoYo.hkrpg",
   "id": 1523037824,
   "name": "崩坏：星穹铁道",
-  "version": "2.4.0",
+  "version": "2.5.0",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/4a/9b/90/4a9b9082-a3bc-e8cb-9063-eae4874cadac/AppIcon-1x_U007emarketing-0-7-0-85-220-0.png/100x100bb.jpg"
 }
