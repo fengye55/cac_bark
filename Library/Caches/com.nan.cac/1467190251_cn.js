5f0ac5595b10b05c2ecefb78efb6d25cbc578e5f
GitHub Action <action@github.com>
2024-10-08 22:28:51 +0000

    save caches

--- Library/Caches/com.nan.cac/1467190251_cn.js
+++ Library/Caches/com.nan.cac/1467190251_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.miHoYo.Yuanshen",
   "id": 1467190251,
   "name": "原神-纳塔开放",
-  "version": "5.0.0",
+  "version": "5.1.0",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/a5/45/69/a5456909-12b9-f710-829c-9b1f8b24ce00/AppIcon-1x_U007epad-0-0-85-220-0.png/100x100bb.jpg"
 }
