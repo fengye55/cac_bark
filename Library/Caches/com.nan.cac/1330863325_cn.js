7a7aab6a1cd6fd5f12d6bd8aabfcd3fc013bb378
GitHub Action <action@github.com>
2024-11-28 03:25:01 +0000

    save caches

--- Library/Caches/com.nan.cac/1330863325_cn.js
+++ Library/Caches/com.nan.cac/1330863325_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.netease.id5",
   "id": 1330863325,
   "name": "第五人格",
-  "version": "24.317.82",
+  "version": "24.331.870",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/3a/7e/04/3a7e040e-defe-0291-6dfb-00ffe7ed6225/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/444934666_cn.js
+++ Library/Caches/com.nan.cac/444934666_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.tencent.mqq",
   "id": 444934666,
   "name": "QQ",
-  "version": "9.1.16",
+  "version": "9.1.20",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/94/8b/f6/948bf614-a6e4-b6be-f81f-4d2e35b49613/AppIcon-1-0-1x_U007epad-0-1-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
